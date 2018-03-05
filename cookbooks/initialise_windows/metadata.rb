6e25c46e7490f322c9c23743b176c446b0ab3694
Connor Conway <connor_conway1@outlook.com>
2018-03-05 09:22:44 +0000

    ch[23] Create initialise windows cookbook with kitchen yml file

---
+++ cookbooks/initialise_windows/metadata.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+name 'initialise_windows'
+description 'A Cookbook install applications to Windows'
+maintainer 'Connor Conway'
+maintainer_email 'connor_conway1@outlook.com'
+license 'Apache-2.0'
+version '1.0.0'
